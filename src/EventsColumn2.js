9b6b2dffbffc45da2318813d407585722d018b7f
Julia Starsy <j.starsy@gmx.de>
2019-11-28 13:51:23 +0100

    Add Events.json to 2nd Colum

--- src/EventsColumn2.js
+++ src/EventsColumn2.js
@@ -1,10 +1,39 @@
+import React from "react";
 import styled from "styled-components";
+import eventData from "./event.json";
 
-const EventsColumn = styled.div`
-  width: 30%;
+export default function EventsColumn() {
+  return (
+    <StyledEventColumn>
+      <h2>WhatsUp today and this week? 🔮 </h2>
+      {eventData.map(event => (
+        <div>
+          <EventList>{event.title}</EventList>
+          Date:{event.day}
+          <EventList>Time:{event.time}</EventList>
+          <p></p>
+        </div>
+      ))}
+    </StyledEventColumn>
+  );
+}
+
+var later = require("later");
+
+var sched = later.parse.text("every 5 mins"),
+  occurrences = later.schedule(sched).next(10);
+
+for (var i = 0; i < 10; i++) {
+  console.log(occurrences[i]);
+}
+
+const StyledEventColumn = styled.div`
+  display: inline-block;
   background: #35682d;
-  height: 100vh;
   margin: 10px;
+  color: white;
 `;
 
-export default EventsColumn;
+const EventList = styled.p`
+  margin: 0px;
+`;
