d4879f2abcd0359d564e0b19d7094e441e74b965
Julia Starsy <j.starsy@gmx.de>
2019-12-03 11:12:25 +0100

    Add Cypress

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,9 @@
+import { exportAllDeclaration } from "@babel/types";
+
+describe("My first Test", function() {
+  it("Finds an element", function() {
+    cy.visit("http://localhost:3000/"
+
+    cy.contains(‘title')
+  })
+})
