36efe8fa904446ffdaa163635fb288390d8badcd
Julia Starsy <j.starsy@gmx.de>
2019-12-03 12:17:41 +0100

    Add Cypress to project

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -2,10 +2,10 @@ import { exportAllDeclaration } from "@babel/types";
 
 describe("My first Test", function() {
   it("Finds an element", function() {
-    cy.visit("http://localhost:3000/"
-    })
+    cy.visit("http://localhost:3000/");
+  });
 
-    it('has the correct title', () => {
-      cy.title().should('equal', 'Buzz Board')
-    })
-  })
+  it("has the correct title", () => {
+    cy.title().should("equal", "Buzz Board");
+  });
+});
