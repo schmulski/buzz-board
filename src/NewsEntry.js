bdfac220bd9f9882baa96c1d06e538dd32a2f4a3
Julia Starsy <j.starsy@gmx.de>
2019-12-10 14:34:38 +0100

    Changes till afternoon

--- src/NewsEntry.js
+++ src/NewsEntry.js
@@ -1,37 +1,62 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled from 'styled-components/macro'
 import * as moment from 'moment'
 import slack from './images/slack.png'
+import Newsticker from 'react-newsticker'
 
-export default function NewsEntry({ user, message, timestamp }) {
+import firebase from 'firebase/app'
+import 'firebase/database'
+import {
+  FirebaseDatabaseProvider,
+  FirebaseDatabaseNode,
+} from '@react-firebase/database'
+
+import FirebaseConfig from './FirebaseConfig'
+
+export default function NewsEntry() {
   return (
-    <ul>
-      <ListStyle>
-        <User> {user} </User>
-        <Message>{message}</Message>
-        <Timestamp> {moment(timestamp).format('LLLL')}</Timestamp>
-        <p></p>
-      </ListStyle>
-    </ul>
+    <FirebaseDatabaseProvider firebase={firebase} {...FirebaseConfig}>
+      <FirebaseDatabaseNode path="news/">
+        <Newsticker>
+          {data => {
+            return (data.value || []).map(news => (
+              <div>
+                {news.title} Date:
+                {news.birthday}
+                {news.timestamp}
+              </div>
+            ))
+          }}
+        </Newsticker>
+      </FirebaseDatabaseNode>
+    </FirebaseDatabaseProvider>
   )
 }
 
-const ListStyle = styled.li`
-  list-style-image: url(${slack});
-  margin: 20px;
-`
+//         <User> {user} </User>
+//         <Message>{message}</Message>
+//         <Timestamp> {moment(timestamp).format('LLLL')}</Timestamp>
+//         <p></p>
 
-const User = styled.p`
-  color: red;
-  font-family: helvetica;
-`
+//   ]
+// }
 
-const Message = styled.span`
-  color: #49fb35;
-  font-family: helvetica;
-`
-const Timestamp = styled.span`
-  font-family: helvetica;
-  font-size: 10px;
-  color: red;
-`
+// const ListStyle = styled.li`
+//   list-style-image: url(${slack});
+//   margin: 20px;
+// `
+
+// const User = styled.p`
+//   color: red;
+//   font-family: helvetica;
+// `
+
+// const Message = styled.span`
+//   color: #49fb35;
+//   font-family: helvetica;
+// `
+// const Timestamp = styled.span`
+//   font-family: helvetica;
+//   font-size: 10px;
+//   color: red;
+//
